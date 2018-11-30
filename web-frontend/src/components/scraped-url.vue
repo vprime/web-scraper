7870e01c96943ee34ee6e719546817d60d62934b
vprime <myself@vincentprime.com>
2018-11-29 18:25:03 -0700

    Adding a button to scrape a detection

--- web-frontend/src/components/scraped-url.vue
+++ web-frontend/src/components/scraped-url.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <a :href="address">{{address}}</a>
+    <a :href="address">{{address}}</a> <button v-on:click="scanUrl(address)">scan</button>
   </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
 export default {
   // name: 'ComponentName',
-  props: ['address']
+  props: ['address'],
+  methods: {
+    ...mapMutations('scrape', ['setUrl']),
+    scanUrl (url) {
+      this.setUrl(url)
+      this.$store.dispatch('scrape/scrape')
+    }
+  }
 }
 </script>
 
